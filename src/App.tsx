d23a23b5e9788536ab043631a3558c8992f6ec1f
Whale2200d <whale2200d@gmail.com>
2023-08-01 21:13:41 +0900

    Refactor : 파일명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Signup from './page/Signup';
-import Signin from './page/Signin';
+import SignUp from './page/SignUp';
+import SignIn from './page/SignIn';
 import Todo from './page/Todo';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Signin />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<SignIn />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/todo" element={<Todo />} />
     </Routes>
   );
