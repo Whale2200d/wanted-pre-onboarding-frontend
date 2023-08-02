979c92c792b9d33d7f0bf5227ae706983b0495f1
Whale2200d <whale2200d@gmail.com>
2023-08-02 20:30:59 +0900

    Feat : localStorage에 todos 저장 외 업데이트

--- src/page/Todo/index.tsx
+++ src/page/Todo/index.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { useState } from "react";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 interface TodoProps {
   userId: number;
@@ -7,43 +7,49 @@ interface TodoProps {
   isCompleted: boolean;
 }
 
-interface TodoArrProps extends Array<TodoProps> {}
-
 function Todo() {
-  const BASE_URL = "https://www.pre-onboarding-selection-task.shop";
-  const access_token = localStorage.getItem("access_token");
-  
+  const BASE_URL = 'https://www.pre-onboarding-selection-task.shop';
+  const access_token = localStorage.getItem('access_token');
+
   const [todos, setTodos] = useState<TodoProps[]>([]);
-  const [inputValue, setInputValue] = useState("");
-  const [isChecked, setIsChecked] = useState<boolean>(false);
-  const [isModify, setIsModify] = useState<boolean>(false);
-  
-  let todoArr: TodoArrProps = [];
-  
+  const [inputValue, setInputValue] = useState('');
+  const [updatingIndex, setUpdatingIndex] = useState<number>(-1);
+  const [updatingValue, setUpdatingValue] = useState<string>('');
+
   const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
-  
+
+  // Todo가 변경될 때마다 localStorage에 저장하기
+  useEffect(() => {
+    window.localStorage.setItem('todoItems', JSON.stringify(todos));
+  }, [todos]);
+
+  // 페이지가 로드될 때마다, localStorage에서 Todo를 가져오기
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todoItems');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+      console.log('JSON.parse(storedTodos): ', JSON.parse(storedTodos));
+    }
+  }, []);
+
   const createTodo = () => {
     const newTodo: TodoProps = {
       userId: todos.length + 1,
       todo: inputValue,
-      isCompleted: isChecked,
+      isCompleted: false,
     };
     setTodos([...todos, newTodo]);
-    setInputValue("");
-  
-    console.log('todoArr: ', todoArr)
-    todoArr.push(newTodo)
-    localStorage.setItem('todoItems', JSON.stringify(todoArr))
-    
+    setInputValue('');
+
     axios({
       url: `${BASE_URL}/todos`,
-      method: "POST",
+      method: 'POST',
       withCredentials: true,
       headers: {
         Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       data: {
         todo: inputValue,
@@ -51,24 +57,21 @@ function Todo() {
     })
       .then((result) => {
         if (result.status === 201) {
-          console.log("Created Todo Data!!");
+          console.log('Created Todo Data!!');
         }
       })
       .catch((error) => {
         console.log(error);
       });
-
-      
   };
 
   const deleteTodo = (id: number) => {
     const updatedTodos = todos.filter((todo) => todo.userId !== id);
     setTodos(updatedTodos);
-    localStorage.setItem('todoItems', JSON.stringify(updatedTodos))
 
     axios({
       url: `${BASE_URL}/todos/:${id}`,
-      method: "DELETE",
+      method: 'DELETE',
       withCredentials: true,
       headers: {
         Authorization: `Bearer ${access_token}`,
@@ -76,7 +79,7 @@ function Todo() {
     })
       .then((result) => {
         if (result.status === 204) {
-          console.log("delete Todo Data!!");
+          console.log('delete Todo Data!!');
         }
       })
       .catch((error) => {
@@ -84,31 +87,62 @@ function Todo() {
       });
   };
 
-  const updateTodo = (id: number, todo: string, isChecked: boolean) => {
+  const startUpdating = (userId: number) => {
+    setUpdatingIndex(userId - 1);
+    setUpdatingValue(todos[userId - 1].todo);
+  };
+
+  const updateTodo = (userId: number, todo: string, isCompleted: boolean) => {
+    const updatedTodos = [...todos];
+    if (updatingIndex !== -1) {
+      updatedTodos[updatingIndex].todo = updatingValue;
+      updatedTodos[updatingIndex].isCompleted = !isCompleted;
+      console.log(
+        'updatedTodos[updatingIndex].todo:',
+        updatedTodos[updatingIndex].todo
+      );
+    }
+
     axios({
-      url: `${BASE_URL}/todos/:${id}`,
-      method: "PUT",
+      url: `${BASE_URL}/todos/:${userId - 1}`,
+      method: 'PUT',
       withCredentials: true,
       headers: {
         Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       data: {
         todo: todo,
-        isCompleted: isChecked,
+        isCompleted: isCompleted,
       },
     })
       .then((result) => {
         if (result.status === 200) {
-          console.log("update Todo Data!!");
+          console.log('update Todo Data!!');
         }
       })
       .catch((error) => {
         console.log(error);
       });
 
-    setIsChecked(!isChecked);
-    setIsModify(!isModify);
+    setTodos(updatedTodos);
+    setUpdatingIndex(-1);
+    setUpdatingValue('');
+  };
+
+  const cancelUpdating = () => {
+    setUpdatingIndex(-1);
+    setUpdatingValue('');
+  };
+
+  const handleToggleComplete = (userId: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.userId === userId
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo
+      )
+    );
   };
 
   return (
@@ -122,20 +156,20 @@ function Todo() {
       <button onClick={createTodo} data-testid="new-todo-add-button">
         추가
       </button>
-      {todos.map((todo, id) => {
+      {todos.map((todo) => {
         return (
           <li key={todo.userId}>
             <label>
-              {!isModify ? (
+              {updatingIndex + 1 !== todo.userId ? (
                 <>
                   <input
                     type="checkbox"
-                    checked={isChecked}
-                    onChange={() => setIsChecked(!isChecked)}
+                    checked={todo.isCompleted}
+                    onChange={() => handleToggleComplete(todo.userId)}
                   />
                   <span>{todo.todo}</span>
                   <button
-                    onClick={() => setIsModify(!isModify)}
+                    onClick={() => startUpdating(todo.userId)}
                     data-testid="modify-button"
                   >
                     수정
@@ -149,11 +183,19 @@ function Todo() {
                 </>
               ) : (
                 <>
-                  <input type="text" defaultValue={todo.todo} />
-                  <button onClick={() => updateTodo(id, todo.todo, isChecked)}>
+                  <input
+                    type="text"
+                    value={updatingValue}
+                    onChange={(e) => setUpdatingValue(e.target.value)}
+                  />
+                  <button
+                    onClick={() =>
+                      updateTodo(todo.userId, todo.todo, todo.isCompleted)
+                    }
+                  >
                     제출
                   </button>
-                  <button onClick={() => setIsModify(!isModify)}>취소</button>
+                  <button onClick={cancelUpdating}>취소</button>
                 </>
               )}
             </label>
